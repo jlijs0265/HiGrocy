9d5a604e84bad6c34fca0c13eda89467c7d7a79f
HuiMun <jlijs0265@gmail.com>
2023-08-01 12:16:02 +0900

    DOU-51 단일 Row 불러오는 함수추가

--- src/main/java/org/zerock/mapper/ProductionMachineMapper.java
+++ src/main/java/org/zerock/mapper/ProductionMachineMapper.java
@@ -7,6 +7,7 @@ import org.zerock.domain.ProductionMachineVO;
 
 public interface ProductionMachineMapper {
 	public List<ProductionMachineVO> getList();
+	public ProductionMachineVO getPM(int machine_code);
 	public boolean insertPM(ProductionMachineVO vo);
 	public boolean updatePM(ProductionMachineVO vo);
 	public boolean deletePM(int machine_code);

254e2ac78ab93973969ebb1909c0d60a3a2d85e0
h2jin <tkdwls1229@naver.com>
2023-08-02 17:51:55 +0900

    DOU-29 생산품 등록, 조회 mapper 작성

---
+++ src/main/java/org/zerock/mapper/ProductMapper.java
@@ -0,0 +1,19 @@
+package org.zerock.mapper;
+
+import java.util.ArrayList;
+
+import org.zerock.domain.BomVO;
+import org.zerock.domain.ItemVO;
+import org.zerock.domain.ProductVO;
+
+public interface ProductMapper {
+	
+	ArrayList<ProductVO> getProductList();
+	boolean registerItem(ItemVO vo);
+	int getMaxItemCode();
+	boolean registerProduct(ProductVO vo);
+	boolean registerBom(BomVO vo);
+	ArrayList<BomVO> getBomList(int product_code);
+	
+
+}

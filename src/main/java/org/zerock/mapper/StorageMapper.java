bd756227beeaf70abb8a9e8aa89b23f74fef52f8
O.Ojinnie <dongjin.dev@gmail.com>
2023-08-02 16:29:24 +0900

    DOU-61 Mapper 연결 및 CRUD SQL 질의문 작성

--- src/main/java/org/zerock/mapper/StorageMapper.java
+++ src/main/java/org/zerock/mapper/StorageMapper.java
@@ -1,5 +1,20 @@
 package org.zerock.mapper;
 
+import java.util.List;
+
+import org.zerock.domain.StorageVO;
+
 public interface StorageMapper {
+	
+	//등록
+	public int register(StorageVO vo);
+	//수정
+	public int update(StorageVO vo);
+	//삭제
+	public int delete(int code);
+	//단일 조회
+	public StorageVO get(int code);
+	//전체 조회
+	public List<StorageVO> getList();
 
 }

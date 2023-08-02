a48175342b8934bec81f77c9412b45c25745086a
PARKMINJUN <junee0530@naver.com>
2023-08-02 12:01:19 +0900

    DOU-45 거래처 DB 연동 Mapper 추가 - 목록 가져오기, insert 구문 완료

---
+++ src/main/java/org/zerock/mapper/accountMapper.java
@@ -0,0 +1,25 @@
+package org.zerock.mapper;
+
+import java.util.List;
+
+import org.zerock.domain.accountVO;
+import org.zerock.domain.orderVO;
+
+public interface accountMapper {
+	
+	// getList
+	public List<accountVO> getList();
+	
+	// read
+	public accountVO read(int account_code);
+	
+	// create
+	public void insert(accountVO account);
+
+	// del
+	public int delete(int account_code);
+	
+	//update
+	public int update(accountVO account);
+	
+}

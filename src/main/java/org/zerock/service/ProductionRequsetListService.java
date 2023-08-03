f90bf0cadbc3ba9648cfca28545b9c0f7b5860ed
HuiMun <jlijs0265@gmail.com>
2023-08-03 20:52:02 +0900

    DOU-48 TODO: 서비스 영역 안넘어감 내일 수정해야함.

--- src/main/java/org/zerock/service/ProductionRequsetListService.java
+++ src/main/java/org/zerock/service/ProductionRequsetListService.java
@@ -9,11 +9,13 @@ import org.springframework.transaction.annotation.Transactional;
 import org.zerock.domain.BetweenDateVO;
 import org.zerock.domain.PRListVO;
 import org.zerock.domain.PRRecordVO;
+import org.zerock.domain.ProductProcessVO;
 import org.zerock.domain.ProductionListVO;
 import org.zerock.domain.ProductionRecordVO;
 import org.zerock.domain.ProductionRequestListVO;
 import org.zerock.mapper.PRListMapper;
 import org.zerock.mapper.PRRecordMapper;
+import org.zerock.mapper.ProductProcessMapper;
 import org.zerock.mapper.ProductionListMapper;
 import org.zerock.mapper.ProductionRecordMapper;
 
@@ -32,6 +34,9 @@ public class ProductionRequsetListService {
 	@Autowired
 	private ProductionRecordMapper p_record_mapper;
 	
+	@Autowired
+	private ProductProcessMapper pp_mapper;
+	
 	//생산요청 현황 조회
 	//조회버튼 눌렀을때 서비스 생산요청 목록 - 생산 목록 해서 (날짜별로)가져와야함
 	public List<ProductionRequestListVO> getProductionList(BetweenDateVO vo){
@@ -40,11 +45,14 @@ public class ProductionRequsetListService {
 	
 	
 	//생산 버튼 눌렀을시 작동할 서비스(트랜잭션화 필요)
-	@Transactional
 	public boolean product(ProductionRecordVO vo) {
+		//TODO:서비스 안넘어옴 수정필요
+		log.info("service....");
+		List<ProductProcessVO> PRlist = pp_mapper.getlist(vo.getItem_code());
 		ProductionListVO list = new ProductionListVO(); 
 		list.setItem_code(vo.getItem_code());
-		list.setPR_code(vo.getPR_code());
-		return p_record_mapper.insertPR(vo) & pl_mapper.insertPL(list);
+		list.setPR_code(vo.getPr_code());
+		log.info(PRlist);
+		return false; //p_record_mapper.insertPR(PRlist) & pl_mapper.insertPL(list);
 	}
 }

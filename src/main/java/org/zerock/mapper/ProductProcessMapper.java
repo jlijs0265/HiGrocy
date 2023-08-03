8d524dbb0e6e84ac463d44d959f6ff6e09430efb
HuiMun <jlijs0265@gmail.com>
2023-08-03 17:18:28 +0900

    DOU-48 생산프로세스 매퍼 해당하는 생산품 코드 파라미터로 받게 수정

--- src/main/java/org/zerock/mapper/ProductProcessMapper.java
+++ src/main/java/org/zerock/mapper/ProductProcessMapper.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.zerock.domain.ProductProcessVO;
 
 public interface ProductProcessMapper {
-	public List<ProductProcessVO> getlist();
+	public List<ProductProcessVO> getlist(int product_code);
 	public boolean insert(ProductProcessVO vo);
 	public boolean update(ProductProcessVO vo);
 	public boolean delete(int product_process_code);

298d3917d91d5228a2de10bda3e7b494ff8826d7
HuiMun <jlijs0265@gmail.com>
2023-08-03 17:17:08 +0900

    DOU-48 생산품 프로세스 서비스 추가

---
+++ src/main/java/org/zerock/service/ProductProcessService.java
@@ -0,0 +1,43 @@
+package org.zerock.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.zerock.domain.ProductProcessVO;
+import org.zerock.domain.ProductionMachineVO;
+import org.zerock.mapper.ProductProcessMapper;
+import org.zerock.mapper.ProductionMachineMapper;
+
+@Service
+public class ProductProcessService {
+	
+	@Autowired
+	private ProductProcessMapper mapper;
+	
+	//생산공정 리스트 조회
+	public List<ProductProcessVO> getlist(int product_code) {
+		return mapper.getlist(product_code);
+	}
+	
+	//생산공정 등록
+	@Transactional
+	public boolean insert(List<ProductProcessVO> vo) {
+		for(ProductProcessVO v : vo) {
+			mapper.insert(v);
+		}
+		return true;
+	}
+	
+	//생산공정 수정
+	public boolean update(ProductProcessVO vo) {
+		return mapper.update(vo);
+	}
+	
+	//생산공정 삭제
+	public boolean delete(int product_process_code) {
+		return mapper.delete(product_process_code);
+	}
+		
+}

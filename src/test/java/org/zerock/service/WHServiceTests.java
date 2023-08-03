51ba03bf3e856d791c9795c3fbe718ca82df6303
O.Ojinnie <dongjin.dev@gmail.com>
2023-08-03 16:22:12 +0900

    DOU-58 Mapper Test & Service Test 완료

---
+++ src/test/java/org/zerock/service/WHServiceTests.java
@@ -0,0 +1,72 @@
+package org.zerock.service;
+
+import static org.junit.Assert.*;
+
+import java.sql.Date;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.zerock.domain.WHListVO;
+
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Log4j
+public class WHServiceTests {
+
+	@Autowired
+	private WHService service;
+	
+	
+	
+	/***************Service Test Complete*********************/
+//	@Test
+//	public void registerWH() {
+//	    WHListVO vo = new WHListVO();
+//	    log.info("registerService Tests...");
+//	
+//	    vo.setWarehousing_record_code(2);
+//	    vo.setItem_code(10);
+//	    vo.setStorage_code(1);
+//	    vo.setWarehousing_type("입고");
+//	    vo.setWr_date(Date.valueOf("2023-08-03")); // Java Time API 사용
+//	    vo.setAmount(100);
+//	    vo.setWarehousing_type_code("P-003");
+//	    vo.setKeeping_date(Date.valueOf("2023-08-03")); // Java Time API 사용
+//	    service.register(vo);
+//	}
+//		@Test
+//		public void updateWH() {
+//			WHListVO vo = new WHListVO();
+//			log.info("updateService Tests...");
+//		
+//		    vo.setWarehousing_record_code(1);
+//		    vo.setItem_code(10);
+//		    vo.setStorage_code(1);
+//		    vo.setWarehousing_type("입고");
+//		    vo.setWr_date(Date.valueOf("2023-08-03")); // Java Time API 사용
+//		    vo.setAmount(100);
+//		    vo.setWarehousing_type_code("P-002");
+//		    vo.setKeeping_date(Date.valueOf("2023-08-05")); // Java Time API 사용
+//		    service.update(vo);
+//		}
+//		@Test
+//		public void deleteWH() {
+//			log.info("deleteService Tests...");
+//			service.delete(2);
+//		}
+//		@Test
+//		public void getWH() {
+//			log.info("getService Tests..."+service.get(1));
+//			
+//		}
+//		@Test
+//		public void getListWH() {
+//			log.info("getListService Tests..."+ service.getList());
+//		}
+
+}

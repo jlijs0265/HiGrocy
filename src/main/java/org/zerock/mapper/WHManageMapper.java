17ceb586a1c2f9108386fbd2d28812fea2cd764c
O.Ojinnie <dongjin.dev@gmail.com>
2023-08-03 16:20:45 +0900

    DOU-58 Mapper 추가

---
+++ src/main/java/org/zerock/mapper/WHManageMapper.java
@@ -0,0 +1,22 @@
+package org.zerock.mapper;
+
+import java.util.List;
+
+import org.zerock.domain.WHListVO;
+
+public interface WHManageMapper {
+	// 등록
+	public int register(WHListVO vo);
+
+	// 수정
+	public int update(WHListVO vo);
+
+	// 삭제
+	public int delete(int code);
+
+	// 단일 조회
+	public WHListVO get(int code);
+
+	// 전체 조회
+	public List<WHListVO> getList();
+}

2d2724d1a72a41b438aaf9c78f4f411af641befe
h2jin <tkdwls1229@naver.com>
2023-08-04 11:13:54 +0900

    DOU-47 생산요청서 품목코드 ajax 통신 controller

--- src/main/java/org/zerock/controller/PRRecordRestController.java
+++ src/main/java/org/zerock/controller/PRRecordRestController.java
@@ -5,9 +5,9 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.zerock.domain.ItemVO;
 import org.zerock.domain.ProductVO;
 import org.zerock.service.PRRecordService;
 
@@ -27,12 +27,12 @@ public class PRRecordRestController {
 		return new ResponseEntity<List<ProductVO>> (productList, HttpStatus.OK);
 	}
 	
-	@GetMapping("/itemAmount")
-	public String getAmount(@RequestBody int item_code) {
+	@PostMapping("/itemAmount")
+	public String getAmount(@RequestBody String code) {
 		// item_code를 받아서 BOM 조회 후, 해당 원재료의 발주목록에 있는 단가를 가져온다.
+		log.info(code);
 		
-		
-		return "success";
+		return "okok";
 	}
 	
 }

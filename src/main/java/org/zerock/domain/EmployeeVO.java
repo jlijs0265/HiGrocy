539b1ea24d36efa3ccef40030fd5f4e17a2df21d
h2jin <tkdwls1229@naver.com>
2023-08-05 15:35:30 +0900

    DOU-70 EmployeeVO 직렬화 추가

--- src/main/java/org/zerock/domain/EmployeeVO.java
+++ src/main/java/org/zerock/domain/EmployeeVO.java
@@ -1,11 +1,12 @@
 package org.zerock.domain;
 
+import java.io.Serializable;
 import java.sql.Date;
 
 import lombok.Data;
 
 @Data
-public class EmployeeVO {
+public class EmployeeVO implements Serializable{
 	
 	private int emp_no;
 	private String empid;

78a49983523c5c48e260c0cf56104eae304c4cc3
h2jin <tkdwls1229@naver.com>
2023-08-06 16:03:02 +0900

    DOU-47 PRRecordVO 직렬화

--- src/main/java/org/zerock/domain/PRRecordVO.java
+++ src/main/java/org/zerock/domain/PRRecordVO.java
@@ -1,14 +1,16 @@
 package org.zerock.domain;
 
+import java.io.Serializable;
 import java.sql.Date;
 
 import lombok.Data;
 
 @Data
-public class PRRecordVO {
+public class PRRecordVO implements Serializable{
 	
 	int pr_record_code;
 	Date request_date;
 	String request_manager;
+	
 	
 }

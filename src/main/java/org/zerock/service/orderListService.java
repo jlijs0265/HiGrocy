d3af6e701ba8f7fd8b3f54ff46c09192c4c19d2c
PARKMINJUN <junee0530@naver.com>
2023-08-06 14:56:39 +0900

    DOU-38 orderListService에 service어노테이션추가

--- src/main/java/org/zerock/service/orderListService.java
+++ src/main/java/org/zerock/service/orderListService.java
@@ -1,8 +1,10 @@
 package org.zerock.service;
 
+import org.springframework.stereotype.Service;
 import org.zerock.domain.ItemVO;
 import org.zerock.domain.orderListVO;
 
+@Service
 public interface orderListService {
 
 	public boolean insertOL(orderListVO vo);

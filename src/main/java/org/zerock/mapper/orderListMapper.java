5daf8fedd9afdfed0ab88f33401949d76bdf2615
HuiMun <jlijs0265@gmail.com>
2023-08-07 17:10:07 +0900

    orderList Mapper order_code 이용하여 orderList 조회하게 추가

--- src/main/java/org/zerock/mapper/orderListMapper.java
+++ src/main/java/org/zerock/mapper/orderListMapper.java
@@ -10,6 +10,7 @@ import org.zerock.domain.orderRecordVO;
 public interface orderListMapper {
 	
 	public boolean insertOL(orderListVO vo);
+	public List<orderListVO> selectItem(int order_code);
 	public List<orderRecordVO> selectBetween(BetweenDateVO vo);
 	
 }
---
+++ src/test/java/org/zerock/service/orderListTests.java
@@ -0,0 +1,35 @@
+package org.zerock.service;
+
+import static org.junit.Assert.*;
+
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.zerock.domain.PMListVO;
+import org.zerock.domain.ProductionMachineVO;
+import org.zerock.domain.RawMaterialVO;
+
+import lombok.extern.log4j.Log4j;
+@RunWith(SpringJUnit4ClassRunner.class)
+
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Log4j
+public class orderListTests {
+	@Autowired
+	private orderListService service;
+	
+	@Test
+	public void test() {
+		log.info(service.selectItem(1));
+	}
+	
+
+
+
+}
